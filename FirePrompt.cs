332d62cc7083f3cd12de4338c42224f60c8c4c6a
Max <maxb@hoger.co.il>
2023-08-07 14:26:56 +0300

    fixes

--- FirePrompt.cs
+++ FirePrompt.cs
@@ -3,109 +3,71 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
 public class FirePrompt
 {
-    private readonly string _fireEngineUrl;
+    private readonly string _url;
     private readonly string _apiKey;
 
-    // Constructor with environment variable-based initialization
+    /// <summary>
+    /// Creates a new FirePrompt instance, using credentials from Environment Variables.
+    /// </summary>
     public FirePrompt()
     {
-        _fireEngineUrl = Environment.GetEnvironmentVariable("FIRE_ENGINE_URL") ?? "";
+        _url = Environment.GetEnvironmentVariable("FIRE_ENGINE_URL") ?? "";
         _apiKey = Environment.GetEnvironmentVariable("FIRE_ENGINE_API_KEY") ?? "";
 
-        if (string.IsNullOrEmpty(_fireEngineUrl) || string.IsNullOrEmpty(_apiKey))
+        if (string.IsNullOrEmpty(_url) || string.IsNullOrEmpty(_apiKey))
         {
             throw new InvalidOperationException("FIRE_ENGINE_URL and FIRE_ENGINE_API_KEY environment variables must be set.");
         }
     }
 
-
-    // Explicit constructor to pass fireEngineUrl and apiKey as arguments
-    public FirePrompt(string fireEngineUrl, string apiKey)
+    /// <summary>
+    /// Creates a new FirePrompt instance, using provided credentials.
+    /// </summary>
+    public FirePrompt(string url, string apiKey)
     {
-        _fireEngineUrl = fireEngineUrl;
+        _url = url;
         _apiKey = apiKey;
     }
 
-    public object Fire(string pipelineId, object input = null, Dictionary<string, object> variables = null)
+    /// <summary>
+    /// Fire (Invoke) a pipeline according to its configurations and dynamic parameters you provide
+    /// </summary>
+    /// <param name="id">Pipeline ID</param>
+    /// <param name="input">User input</param>
+    /// <param name="variables">Other variables that will compile with both System and User inputs</param>
+    public async Task<object> FireAsync(
+    string id,
+    object input = null,
+    Dictionary<string, string> variables = null)
     {
-        // Synchronous method to make the HTTP request
         using (HttpClient httpClient = new HttpClient())
         {
             httpClient.DefaultRequestHeaders.Add("Api-Key", _apiKey);
 
-            string url = $"{_fireEngineUrl}/pipelines/fire?pipelineId={pipelineId}";
+            string url = $"{_url}/Fire";
 
-            if (input != null)
+            // Create the object to be sent
+            var payload = new
             {
-                // You can customize the content based on your requirements here
-                // For simplicity, let's assume we send the input as JSON
-                string jsonInput = Newtonsoft.Json.JsonConvert.SerializeObject(input);
-                StringContent content = new StringContent(jsonInput, Encoding.UTF8, "application/json");
-                httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
+                id = id,
+                input = input,
+                variables = variables
+            };
 
-                HttpResponseMessage response = httpClient.PostAsync(url, content).Result;
-                response.EnsureSuccessStatusCode();
+            // Serialize the object to JSON
+            string jsonPayload = Newtonsoft.Json.JsonConvert.SerializeObject(payload);
+            StringContent content = new StringContent(jsonPayload, Encoding.UTF8, "application/json");
 
-                string responseString = response.Content.ReadAsStringAsync().Result;
+            // Send the POST request with the JSON content
+            HttpResponseMessage response = await httpClient.PostAsync(url, content);
+            response.EnsureSuccessStatusCode();
 
-                // Here, you can handle different response types based on the content type
-                // For simplicity, we will return the response as a string
-                return responseString;
-            }
-            else
-            {
-                HttpResponseMessage response = httpClient.GetAsync(url).Result;
-                response.EnsureSuccessStatusCode();
+            string responseString = await response.Content.ReadAsStringAsync();
 
-                string responseString = response.Content.ReadAsStringAsync().Result;
-
-                // Here, you can handle different response types based on the content type
-                // For simplicity, we will return the response as a string
-                return responseString;
-            }
-        }
-    }
-
-    public async Task<object> FireAsync(string pipelineId, object input = null, Dictionary<string, object> variables = null)
-    {
-        // Asynchronous method to make the HTTP request
-        using (HttpClient httpClient = new HttpClient())
-        {
-            httpClient.DefaultRequestHeaders.Add("Api-Key", _apiKey);
-
-            string url = $"{_fireEngineUrl}/pipelines/fire?pipelineId={pipelineId}";
-
-            if (input != null)
-            {
-                // You can customize the content based on your requirements here
-                // For simplicity, let's assume we send the input as JSON
-                string jsonInput = Newtonsoft.Json.JsonConvert.SerializeObject(input);
-                StringContent content = new StringContent(jsonInput, Encoding.UTF8, "application/json");
-                httpClient.DefaultRequestHeaders.Add("Content-Type", "application/json");
-
-                HttpResponseMessage response = await httpClient.PostAsync(url, content);
-                response.EnsureSuccessStatusCode();
-
-                string responseString = await response.Content.ReadAsStringAsync();
-
-                // Here, you can handle different response types based on the content type
-                // For simplicity, we will return the response as a string
-                return responseString;
-            }
-            else
-            {
-                HttpResponseMessage response = await httpClient.GetAsync(url);
-                response.EnsureSuccessStatusCode();
-
-                string responseString = await response.Content.ReadAsStringAsync();
-
-                // Here, you can handle different response types based on the content type
-                // For simplicity, we will return the response as a string
-                return responseString;
-            }
+            return responseString;
         }
     }
 }
+
